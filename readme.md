0641e93272048a6523eb010d75849d0646d6e467
Seong <sryoo6@gatech.edu>
2020-05-13 18:04:55 -0400

    Upload strudel

---
+++ readme.md
@@ -0,0 +1,61 @@
+# strudel.js
+
+A simple way to handle DOM element focus and aria rules! <3
+
+Having complex interactive elements on a web page can make it difficult to manually update `tabindex` and `aria-hidden`. But updating these attributes is super important for making any website accessible to people using keyboard or voice controls! strudel.js lets you define rules for tabindex and aria visibility *once* instead of dealing with a web of event listeners. 
+
+## Quick example
+
+```javascript
+strudel.query(() => {
+	return strudel.hasClass('.menu', 'opened');
+  })
+  .else()
+  .watch('.menu', 'class')
+  .reaction('.burger', 'focusable');
+```
+This code block will automatically change all DOM elements with class 'burger' to be focusable when the `.menu` element has the class `opened`. The call to `.else()` tells strudel to apply the opposite condition (i.e. change burger elements to be unfocusable) in the case of the condition *not* being met.
+
+
+## Model
+
+strudel's syntax is based off of the syntax of a typical CSS media query: 
+
+```css
+@media screen and (max-width: 48em) {
+  .burger {
+    display: block;
+  }
+}
+```
+That is, you define:
+1. a part of the web page to monitor for change
+  - e.g. `.watch('.menu', 'class')` in strudel, `screen` in css
+2. a specific condition to satisfy
+  - e.g. the anonymous function passed into the `query()` call in strudel,  `max-width: 48em` in css
+3. and a functionality which is applied only if the condition is satisfied.
+  - e.g. `.reaction('.burger', 'focusable')` in strudel, `.burger` styling in css
+  
+## Reference
+
+`strudel.query(conditionFunction)`
+Set a new focus/aria rule and pass in a condition function.
+
+`.else()`
+Add anywhere to a `query()` call chain to specify that the opposite behavior should be applied if the condition is not met.
+
+`.watch(selector, attributeName)`
+Creates a MutationObserver object which waits until the `attributeName` attribute in the selected element changes. Note! If you specify a selector with multiple valid elements (e.g. using class name selector), then strudel will only listen for changes in the first qualifying element.
+
+`.reaction(selector, behavior)`
+There are currently four behaviors to choose from: 
+  - `focusable`, which sets an element's `tabindex=0`
+  - `unfocusable`, which sets an element's `tabindex=-1`
+  - `ariavisible`, which removes an element's `aria-hidden=true` attribute
+  - `ariahidden`, which adds an `aria-hidden=true` attribute
+  
+You can make chains to your heart's desire! For instance, you might chain multiple `.watch()` calls to define multiple different relevant elements to be monitored. You could also add multiple `.reaction` calls to have many kinds of elements react to a single condition in different ways.
+
+## Discussion
+
+This tool operates on the assumption that assigning `tabindex` and `aria-hidden` values is a device and browser-compatible method for engaging with the Accessibility API. This may change over time, and it may change depending on the kind of technology that your audience uses. The reader is referred to libraries like [ally.js](https://allyjs.io/), which more thoroughly handle device compatibility concerns related to `tabindex` and `aria-hidden`.
---
+++ strudel.js
@@ -0,0 +1,155 @@
+function Strudel() {
+  /* Basic helper methods which define important functionality. */
+  /* The developer is encouraged to modify these as needed. */
+  
+  this.makeFocusable = function (element) {
+    element.setAttribute('tabIndex', 0);
+  }
+  this.makeNotFocusable = function (element) {
+    element.setAttribute('tabIndex', -1);
+  }
+  this.makeAria = function (element) {
+    element.removeAttribute('aria-hidden');
+  }
+  this.makeNotAria = function (element) {
+    element.setAttribute('aria-hidden', 'true');
+  }
+
+  this.query = function (conditionFunction) {
+    let strudelQuery = new StrudelQuery(conditionFunction);
+    return strudelQuery;
+  }
+  
+  this.attrCheck = function(selector, attribute, value) {
+    let element = document.querySelectorAll(selector)[0];
+    return element.getAttribute(attribute) == value;
+  }
+  
+  this.hasClass = function(selector, className) {
+    let element = document.querySelectorAll(selector)[0];
+    return element.classList.contains(className);
+  }
+  
+  this.styleCheck = function(selector, style, value) {
+    let element = document.querySelectorAll(selector)[0];
+    return getComputedStyle(element)[style] == value;
+  }
+}
+
+function StrudelRouter() {
+  this.processBehavior = function(behaviorString) {
+    if (behaviorString == 'focusable') {
+      return strudel.makeFocusable;
+    } else if (behaviorString == 'unfocusable') {
+      return strudel.makeNotFocusable;
+    } else if (behaviorString == 'ariahidden') {
+      return strudel.makeNotAria;
+    } else if (behaviorString == 'ariavisible') {
+      return strudel.makeAria;
+    } else {
+      console.log('Please make sure to use a valid behavior name!');
+      return;
+    }
+  }
+
+  this.oppositeBehavior = function(behaviorString) {
+    if (behaviorString == 'focusable') {
+      return strudel.makeNotFocusable;
+    } else if (behaviorString == 'unfocusable') {
+      return strudel.makeFocusable;
+    } else if (behaviorString == 'ariahidden') {
+      return strudel.makeAria;
+    } else if (behaviorString == 'ariavisible') {
+      return strudel.makeNotAria;
+    } else {
+      console.log('Please make sure to use a valid behavior name!');
+      return;
+    }
+  }
+}
+
+const strudel = new Strudel();
+const router = new StrudelRouter();
+
+function StrudelQuery(condition) {
+  this.condition = condition;
+  this.checkElseCondition = false;
+  this.observers = new Array();
+  this.reactors = new Array();
+  this.validBehaviors = ['focusable', 'unfocusable', 'ariahidden', 'ariavisible'];
+  this.opposites = ['unfocusable', 'focusable', 'ariavisible', 'ariahidden'];
+  const self = this;
+
+  this.else = function () {
+    this.checkElseCondition = true;
+    return this;
+  }
+  
+  this.allReact = function () {
+    console.log("CHANGE REGISTERED");
+    let conditionSatisfied = this.condition();
+    for (let i = 0; i < this.reactors.length; i++) {
+      let reactor = this.reactors[i];
+      if (conditionSatisfied) {
+        reactor.act();
+      } else if (this.checkElseCondition) {
+        reactor.actNot();
+      }
+    }
+  }
+  
+  this.watch = function (actor, attributeName) {
+    let callback = function (mutations, observer) {
+      for (let i = 0; i < mutations.length; i++) {
+        let mutation = mutations[i];
+        if (mutation.type === 'attributes' &&
+          mutation.attributeName == attributeName) {
+          self.allReact();
+          break;
+        }
+      }
+    }
+    let observer = new MutationObserver(callback);
+    let target = document.querySelectorAll(actor)[0];
+    let options = {
+      attributes: true,
+      attributeFilter: [attributeName],
+    }
+
+    observer.observe(target, options);
+    this.observers.push(observer);
+
+    return this;
+  }
+  
+
+  
+  this.reaction = function(reactor, behavior) {
+    let behaviorFunction = router.processBehavior(behavior);
+    let oppositeFunction = router.oppositeBehavior(behavior);
+    let rxn = new StrudelReaction(reactor, behaviorFunction, oppositeFunction);
+    this.reactors.push(rxn);
+    return this;
+  }
+  
+}
+
+function StrudelReaction(reactor, behaviorFunction, oppositeFunction) {
+  this.behaviorFunction = behaviorFunction;
+  this.reactors = document.querySelectorAll(reactor);
+  this.oppositeFunction = oppositeFunction;
+  
+  this.act = function () {
+    for (let i = 0; i < this.reactors.length; i++) {
+      let element = this.reactors[i];
+      this.behaviorFunction(element);
+    }
+  }
+
+  this.actNot = function () {
+    for (let i = 0; i < this.reactors.length; i++) {
+      let element = this.reactors[i];
+      this.oppositeFunction(element);
+    }
+  }
+}
