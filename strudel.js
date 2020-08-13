51066995d0fd5bafed42140e6f64f4c576bb6c64
Seong <sryoo6@gatech.edu>
2020-08-12 20:44:30 -0400

    Update syntax and improve clickPress functionality to wait for dom load

--- strudel.js
+++ strudel.js
@@ -1,60 +1,67 @@
+/**
+ * Strudel object
+ */
 function Strudel() {
-  this.query = function (conditionFunction) {
+  this.query = function(conditionFunction) {
     return new StrudelQuery(conditionFunction);
   };
 
-  this.isAttr = function (selector, attribute, value) {
-    let element = document.querySelectorAll(selector)[0];
+  this.isAttr = function(selector, attribute, value) {
+    const element = document.querySelectorAll(selector)[0];
     return element.getAttribute(attribute) == value;
   };
 
-  this.hasAttr = function (selector, attribute) {
-    let element = document.querySelectorAll(selector)[0];
+  this.hasAttr = function(selector, attribute) {
+    const element = document.querySelectorAll(selector)[0];
     return element.hasAttribute(attribute);
   };
 
-  this.hasClass = function (selector, className) {
-    let element = document.querySelectorAll(selector)[0];
+  this.hasClass = function(selector, className) {
+    const element = document.querySelectorAll(selector)[0];
     return element.classList.contains(className);
   };
 
-  this.isStyle = function (selector, style, value) {
-    let element = document.querySelectorAll(selector)[0];
+  this.isStyle = function(selector, style, value) {
+    const element = document.querySelectorAll(selector)[0];
     return getComputedStyle(element)[style] == value;
   };
 
-  this.clickPress = function (selector, callback) {
-    let elements = document.querySelectorAll(selector);
-    
-    for (let i = 0; i < elements.length; i++) {
-      let element = elements[i];
-      element.addEventListener('click', callback);
-      element.addEventListener('keydown', function (event) {
-
-        if (event.key == " " || event.key == "Spacebar") {
-          event.preventDefault();
-          callback();
-        }
-        if (event.key == "Enter") {
-          callback();
-        }
-        
-      });
-    }
-    
+  this.clickPress = function(selector, callback) {
+    const theFunction = function(selector, callback) {
+      const elements = document.querySelectorAll(selector);
+      for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        element.addEventListener('click', callback);
+        element.addEventListener('keydown', function(event) {
+          if (event.key == ' ' || event.key == 'Spacebar') {
+            event.preventDefault();
+            callback(event);
+          }
+          if (event.key == 'Enter') {
+            callback(event);
+          }
+        });
+      }
+    };
+    window.addEventListener('DOMContentLoaded', function(event) {
+      theFunction(selector, callback);
+    });
   };
-  
 }
 
-const strudel = new Strudel();
-
+/**
+ * Class def for obj created by a strudel.query call
+ * @param {function} conditionFunction a boolean function
+ *   representing the case that must be satisfied to activate
+ *   a certain behavior
+ */
 function StrudelQuery(conditionFunction) {
   const self = this;
   this.conditionFunction = conditionFunction;
-  this.observers = new Array();
-  this.reactions = new Array();
+  this.observers = [];
+  this.reactions = [];
 
-  this.getLastReaction = function () {
+  this.getLastReaction = function() {
     return this.reactions[this.reactions.length - 1];
   };
 
@@ -62,29 +69,29 @@ function StrudelQuery(conditionFunction) {
   the only ones that will be used as part of the
   client api */
 
-  this.else = function () {
-    let lastReaction = this.getLastReaction();
+  this.else = function() {
+    const lastReaction = this.getLastReaction();
     lastReaction.doNegative = true;
     return this;
   };
 
-  this.watch = function (actor, attributeName) {
-    let callback = function (mutations, observer) {
+  this.watch = function(actor, attributeName) {
+    const callback = function(mutations, observer) {
       for (let i = 0; i < mutations.length; i++) {
-        let mutation = mutations[i];
-        if (mutation.type === "attributes" &&
+        const mutation = mutations[i];
+        if (mutation.type === 'attributes' &&
           mutation.attributeName == attributeName) {
           self.allReact();
           break;
         }
       }
-    }
-    let observer = new MutationObserver(callback);
-    let target = document.querySelectorAll(actor)[0];
-    let options = {
+    };
+    const observer = new MutationObserver(callback);
+    const target = document.querySelectorAll(actor)[0];
+    const options = {
       attributes: true,
       attributeFilter: [attributeName],
-    }
+    };
 
     observer.observe(target, options);
     this.observers.push(observer);
@@ -92,16 +99,16 @@ function StrudelQuery(conditionFunction) {
     return this;
   };
 
-  this.reaction = function (selector) {
-    let rxn = new StrudelReaction(selector);
+  this.reaction = function(selector) {
+    const rxn = new StrudelReaction(selector);
     this.reactions.push(rxn);
 
     return this;
   };
 
-  this.set = function (attribute, value) {
-    let lastReaction = this.getLastReaction();
-    let action = new StrudelAction("set", attribute, value);
+  this.set = function(attribute, value) {
+    const lastReaction = this.getLastReaction();
+    const action = new StrudelAction('set', attribute, value);
     if (!lastReaction.doNegative) {
       lastReaction.addPos(action);
     } else {
@@ -111,9 +118,9 @@ function StrudelQuery(conditionFunction) {
     return this;
   };
 
-  this.add = function (attribute) {
-    let lastReaction = this.getLastReaction();
-    let action = new StrudelAction("add", attribute, "none");
+  this.add = function(attribute) {
+    const lastReaction = this.getLastReaction();
+    const action = new StrudelAction('add', attribute, 'none');
     if (!lastReaction.doNegative) {
       lastReaction.addPos(action);
     } else {
@@ -124,9 +131,9 @@ function StrudelQuery(conditionFunction) {
   };
 
 
-  this.remove = function (attribute) {
-    let lastReaction = this.getLastReaction();
-    let action = new StrudelAction("remove", attribute, "none");
+  this.remove = function(attribute) {
+    const lastReaction = this.getLastReaction();
+    const action = new StrudelAction('remove', attribute, 'none');
     if (!lastReaction.doNegative) {
       lastReaction.addPos(action);
     } else {
@@ -139,10 +146,10 @@ function StrudelQuery(conditionFunction) {
 
   /* Here ends the main client api methods */
 
-  this.allReact = function () {
-    let cond = this.conditionFunction();
+  this.allReact = function() {
+    const cond = this.conditionFunction();
     for (let i = 0; i < this.reactions.length; i++) {
-      let reaction = this.reactions[i];
+      const reaction = this.reactions[i];
       if (cond) {
         reaction.doPositiveActions();
       } else if (reaction.doNegative) {
@@ -150,55 +157,67 @@ function StrudelQuery(conditionFunction) {
       }
     }
   };
-
 }
 
+/**
+ * Class def representing a group of actions
+ *   that strudel can apply to the DOM.
+ * @param {string} selector string representing elements
+ *   which are to be acted upon
+ */
 function StrudelReaction(selector) {
   this.selector = selector;
-  this.positiveActions = new Array();
-  this.negativeActions = new Array();
+  this.positiveActions = [];
+  this.negativeActions = [];
   this.doNegative = false;
 
-  this.addPos = function (strudelAction) {
+  this.addPos = function(strudelAction) {
     this.positiveActions.push(strudelAction);
   };
 
-  this.addNeg = function (strudelAction) {
+  this.addNeg = function(strudelAction) {
     this.negativeActions.push(strudelAction);
   };
 
-  this.doPositiveActions = function () {
+  this.doPositiveActions = function() {
     this.doActions(this.positiveActions);
   };
 
-  this.doNegativeActions = function () {
+  this.doNegativeActions = function() {
     this.doActions(this.negativeActions);
   };
 
-  this.doActions = function (actions) {
-    let elements = document.querySelectorAll(selector);
+  this.doActions = function(actions) {
+    const elements = document.querySelectorAll(selector);
     for (let i = 0; i < actions.length; i++) {
-      let doAction = actions[i];
+      const doAction = actions[i];
       for (let j = 0; j < elements.length; j++) {
-        let element = elements[j];
-        if (doAction.type == "add") {
+        const element = elements[j];
+        if (doAction.type == 'add') {
           element.setAttribute(doAction.attribute, true);
-        } else if (doAction.type == "set") {
+        } else if (doAction.type == 'set') {
           element.setAttribute(doAction.attribute, doAction.value);
-        } else if (doAction.type == "remove") {
+        } else if (doAction.type == 'remove') {
           element.removeAttribute(doAction.attribute);
         } else {
-          console.log("Strudel: Unrecognized action type of " + doAction.type);
+          console.log('Strudel: Unrecognized action type of ' + doAction.type);
         }
       }
     }
   };
-
-
 }
 
+/**
+ * Wrapper for a strudel action.
+ * @param {string} type      add, set, or remove
+ * @param {string} attribute string compatible with setAttribute js function
+ * @param {*}      value     new value
+ */
 function StrudelAction(type, attribute, value) {
   this.type = type;
   this.attribute = attribute;
   this.value = value;
 }
+
+const strudel = new Strudel();
+window.strudel = strudel;
